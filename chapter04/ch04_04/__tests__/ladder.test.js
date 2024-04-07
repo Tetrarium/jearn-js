887590856b459943b309c45d6ae8d06a6d89cafc
AlexandrTurov <alexandr.turov@mail.ru>
2024-04-08 00:53:17 +0700

    Глава 4.4 Методы объекта, this. Решена задача 2. Написаны тесты

--- chapter04/ch04_04/__tests__/ladder.test.js
+++ chapter04/ch04_04/__tests__/ladder.test.js
@@ -1,3 +1,50 @@
 const { ladder } = require('../tasks');
+global.alert = jest.fn();
 
+describe('test ladder', () => {
+  beforeEach(() => {
+    ladder.step = 0;
+  });
 
+  test('up() должен возвращать this', () => {
+    expect(ladder.up()).toEqual(ladder);
+  });
+
+  test('down() должен возвращать this', () => {
+    expect(ladder.down()).toEqual(ladder);
+  });
+
+  test('showStep() должен вызывать alert', () => {
+    ladder.showStep();
+    expect(global.alert).toHaveBeenCalledWith(0);
+  });
+
+  test('up() должен увеличивать ступеньку', () => {
+    expect(ladder.up().up().step).toBe(2);
+  });
+
+  test('down() должен уменьшать ступеньку', () => {
+    expect(ladder.down().step).toBe(-1);
+  });
+
+  test('down().up().up().up()', () => {
+    expect(ladder.down().up().up().up().step).toBe(2);
+  });
+
+  test('showStep() должен возвращать this', () => {
+    expect(ladder.showStep()).toEqual(ladder);
+  });
+
+  test('up().up().down().showStep().down().showStep()', () => {
+    expect(
+      ladder
+        .up()
+        .up()
+        .down()
+        .showStep()
+        .down()
+        .showStep()
+        .step
+    ).toBe(0);
+  });
+});
