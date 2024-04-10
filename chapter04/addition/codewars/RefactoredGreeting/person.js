a29c68a0e17cc158d46ebc3fb05c3c2aab3605af
AlexandrTurov <alexandr.turov@mail.ru>
2024-04-11 01:00:55 +0700

    Задачи с codewars. Refactored Greeting. Добавлено описание

--- chapter04/addition/codewars/RefactoredGreeting/person.js
+++ chapter04/addition/codewars/RefactoredGreeting/person.js
@@ -1,3 +1,17 @@
+/**
+ * https://www.codewars.com/kata/5121303128ef4b495f000001
+ * 
+ * function greet(myName, yourName) {
+ * return `Hello ${yourName}, my name is ${myName}`;
+ * }
+ * 
+ * Добавить констрктор, чтобы работало следующим образом
+ */
+
+// var joe = new Person('Joe');
+// joe.greet('Kate'); // should return 'Hello Kate, my name is Joe'
+// joe.name;           // should == 'Joe
+
 function greet(myName, yourName) {
   return `Привет ${yourName}, меня зовут ${myName}`;
 }
