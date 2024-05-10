61e32c5f7bb9b0adea5b3afdf9812a8818b3b189
AlexandrTurov <alexandr.turov@mail.ru>
2024-05-10 11:00:47 +0700

    add chapter 5.2

--- chapter05/ch05_01/main.js
+++ chapter05/ch05_01/main.js
@@ -1,6 +1,8 @@
+// 'use strict';
+
 const str = 'Hello!';
 
-str.test = 5;
+str.test = 5; // TypeError when 'use strict'
 
 console.log(str);
 console.log(str.test);
---
+++ chapter05/ch05_02/main.js
@@ -0,0 +1,28 @@
+// 'use strict';
+
+// console.log(1e9);
+// console.log(7.3e5);
+
+// console.log(1.5e-6);
+
+// console.log(0xff);
+// console.log(0b10101);
+// console.log(0o377);
+
+// const num = 255;
+// console.log(num.toString(16));
+// console.log(num.toString(2));
+// console.log(num.toString(8));
+
+// const addr = 'youtube';
+// let cypher = (+addr.replace(/./gi, (char) => char.charCodeAt())).toString(32);
+// console.log(cypher);
+
+// console.log(0.1.toFixed(20));
+// console.log(0.2.toFixed(20));
+// console.log(0.3.toFixed(20));
+// console.log(0.1 + 0.3);
+// console.log(0.1 + 0.2);
+// console.log(0.4.toFixed(20));
+
+// console.log(9_999_999_999_999_999);
