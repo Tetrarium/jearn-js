ff82a270c16a8c4316fa57a01689d71b78e43b5b
AlexandrTurov <alexandr.turov@mail.ru>
2024-06-14 15:23:20 +0700

    LearnJS 5.5 Task 10 is solved

--- chapter05/ch05_05/tasks.js
+++ chapter05/ch05_05/tasks.js
@@ -11,19 +11,20 @@ export function camelize(str) {
 
 // 2. Сортировать в порядке по убыванию
 // https://learn.javascript.ru/array-methods#sortirovat-v-poryadke-po-ubyvaniyu
-(() => {
+function task05_05_02() {
   console.log('task 1');
   const arr = [5, 2, 1, -10, 8];
 
   arr.sort((el1, el2) => el2 - el1);
 
   console.log(arr);
-})();
+}
+// task05_05_02();
 
 
 // 3. Скопировать и отсортировать массив
 // https://learn.javascript.ru/array-methods#skopirovat-i-otsortirovat-massiv
-(() => {
+function task05_05_03() {
   console.log('task2');
   const arr = ["HTML", "JavaScript", "CSS"];
 
@@ -36,7 +37,8 @@ export function camelize(str) {
 
   console.log(sorted);
   console.log(arr);
-})();
+}
+// task05_05_03();
 
 
 // 4. Создать расширяемый калькулятор
@@ -89,6 +91,7 @@ export function filterRange(arr, a, b) {
 // 7 Трансформировать в массив имён
 // https://learn.javascript.ru/array-methods#transformirovat-v-massiv-imyon
 function task05_05_07() {
+  console.log('task 7');
   const vasya = { name: "Вася", age: 25 };
   const petya = { name: "Петя", age: 30 };
   const masha = { name: "Маша", age: 28 };
@@ -105,6 +108,7 @@ function task05_05_07() {
 // 8 Трансформировать в объекты
 // https://learn.javascript.ru/array-methods#transformirovat-v-obekty
 function task05_05_08() {
+  console.log('task 8');
   const petya = { name: "Петя", surname: "Иванов", id: 2 };
   const vasya = { name: "Вася", surname: "Пупкин", id: 1 };
   const masha = { name: "Маша", surname: "Петрова", id: 3 };
@@ -126,6 +130,7 @@ function task05_05_08() {
 // 9 Отсортировать пользователей по возрасту
 // https://learn.javascript.ru/array-methods#otsortirovat-polzovateley-po-vozrastu
 function task05_05_09() {
+  console.log('task 9');
   const vasya = { name: "Вася", age: 25 };
   const petya = { name: "Петя", age: 30 };
   const masha = { name: "Маша", age: 28 };
@@ -140,4 +145,60 @@ function task05_05_09() {
   sortByAge(users);
   console.log(users);
 }
-task05_05_09();
+// task05_05_09();
+
+// 10 Перемешайте массив
+// https://learn.javascript.ru/array-methods#peremeshayte-massiv
+// Сам не решил. Подсмотрел
+function task05_05_10() {
+  function testShiffle(fn) {
+    const count = {};
+
+    const timestamp = performance.now();
+    for (let i = 0; i < 20_000_000; i++) {
+      const arr = [1, 2, 3];
+      fn(arr);
+
+      const key = arr.join('');
+      if (count[key]) {
+        count[key]++;
+      } else {
+        count[key] = 1;
+      }
+    }
+    console.log('Time: ', performance.now() - timestamp, 'ms');
+
+    let result = '';
+    for (let key in count) {
+      result += `${key}: ${count[key]};\n`;
+    }
+
+    console.log(result);
+  }
+
+  function task10_1() {
+    function shiffle(arr) {
+      arr.sort(() => Math.random() - 0.5);
+    }
+
+    const arr = [1, 2, 3];
+    // shiffle(arr);
+    // console.log(arr);
+    testShiffle(shiffle);
+  }
+  task10_1();
+
+  function task10_2() {
+    function shiffle(arr) {
+      for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+
+    }
+
+    testShiffle(shiffle);
+  }
+  task10_2();
+}
+// task05_05_10();
