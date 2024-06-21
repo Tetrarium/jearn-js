6f0a58d3fbca889f19c0b37320082efa17a4bbcf
AlexandrTurov <alexandr.turov@mail.ru>
2024-06-22 01:52:56 +0700

    Chapter 5.8 Task 2 is solved

--- chapter05/ch05_08/tasks.js
+++ chapter05/ch05_08/tasks.js
@@ -8,22 +8,22 @@ function task5_8_1() {
     { text: "See you soon", from: "Alice" }
   ];
 
-  const isReadingStore = new WeakSet();
+  const isReadStore = new WeakSet();
 
   function showMessage(num) {
     const msg = messages[num];
     if (msg) {
-      isReadingStore.add(msg);
+      isReadStore.add(msg);
       console.log(`${msg.from} написал(а): ${msg.text}`);
     } else {
       console.log('Сообщения не существует');
     }
   }
 
-  function showIsMessageReading(num) {
+  function showIsMessageRead(num) {
     const msg = messages[num];
     if (msg) {
-      const txt = isReadingStore.has(msg)
+      const txt = isReadStore.has(msg)
         ? 'прочитано'
         : 'не прочитано';
 
@@ -43,13 +43,72 @@ function task5_8_1() {
     }
   }
 
-  showIsMessageReading(2);
+  showIsMessageRead(2);
   showMessage(2);
   deleteMessage(2);
-  showIsMessageReading(2);
-  showIsMessageReading(1);
-  showIsMessageReading(0);
+  showIsMessageRead(2);
+  showIsMessageRead(1);
+  showIsMessageRead(0);
   showMessage(0);
-  showIsMessageReading(0);
+  showIsMessageRead(0);
 }
 task5_8_1();
+
+
+// 2. Хранение времени прочтения
+// https://learn.javascript.ru/weakmap-weakset#hranenie-vremeni-prochteniya
+
+function task5_8_2() {
+  const messages = [
+    { text: "Hello", from: "John" },
+    { text: "How goes?", from: "John" },
+    { text: "See you soon", from: "Alice" }
+  ];
+
+  const dateReadStore = new WeakMap();
+
+  function showMessage(num) {
+    const msg = messages[num];
+    if (msg) {
+      if (!dateReadStore.has(msg)) {
+        dateReadStore.set(msg, new Date().toLocaleString());
+      }
+      console.log(`${msg.from} написал(а): ${msg.text}`);
+    } else {
+      console.log('Сообщения не существует');
+    }
+  }
+
+  function showWhenMessageRead(num) {
+    const msg = messages[num];
+    if (msg) {
+      const txt = dateReadStore.has(msg)
+        ? `прочитано ${dateReadStore.get(msg)}`
+        : 'не прочитано';
+
+      console.log(`Сообщение от ${msg.from} ${txt}`);
+    } else {
+      console.log('Сообщения не существует');
+    }
+  }
+
+  function deleteMessage(num) {
+    const msg = messages[num];
+    if (num >= messages.length) {
+      console.log(`Сообщения с номером ${num} не существует`);
+    } else {
+      messages.splice(num, 1);
+      console.log('Сообщение удалено');
+    }
+  }
+
+  showWhenMessageRead(2);
+  showMessage(2);
+  deleteMessage(2);
+  showWhenMessageRead(2);
+  showWhenMessageRead(1);
+  showWhenMessageRead(0);
+  showMessage(0);
+  showWhenMessageRead(0);
+}
+task5_8_2();
