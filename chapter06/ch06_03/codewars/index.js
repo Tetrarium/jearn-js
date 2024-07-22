8d9c79019c45484891eda659f2d7258690e7cee8
AlexandrTurov <alexandr.turov@mail.ru>
2024-07-22 16:40:08 +0700

    Codewars. task2

--- chapter06/ch06_03/codewars/index.js
+++ chapter06/ch06_03/codewars/index.js
@@ -14,3 +14,30 @@ function transpose(matrix) {
   return result;
 }
 
+
+// 2. Quantum Bogosort
+// https://www.codewars.com/kata/53165ceea8fbdb4e0c00015b/train/javascript
+
+Array.prototype.qshuffle = function() {
+  for (let i = this.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [this[i], this[j]] = [this[j], this[i]];
+  }
+}
+
+const QSC = {
+  destroyed: false,
+  destroyUniverse() {
+    this.destroyed = true;
+  }
+}
+
+function isSorted(arr) {
+  return arr.every((v, i) => v >= (arr[i - 1] ?? -Infinity));
+}
+
+Array.prototype.qbsort = function() {
+  this.qshuffle();
+  return isSorted(this) ? this : QSC.destroyUniverse();
+}
