2eab0bbc69a2b9c7e5e0ff6ecf609140aa11a31e
AlexandrTurov <alexandr.turov@mail.ru>
2024-07-23 01:43:16 +0700

    Codewars. task3

--- chapter06/ch06_03/codewars/index.js
+++ chapter06/ch06_03/codewars/index.js
@@ -41,3 +41,78 @@ Array.prototype.qbsort = function() {
   this.qshuffle();
   return isSorted(this) ? this : QSC.destroyUniverse();
 }
+
+
+// 3. Texting with an old-school mobile phone
+// https://www.codewars.com/kata/5ca24526b534ce0018a137b5/train/javascript/669e390955f347233f1a1141
+function isLowerCase(char) {
+  return char === char.toLowerCase();
+}
+
+function getKeyPress(char, keyboard) {
+  let keypress = '';
+  let times = 0;
+  char = char.toLowerCase();
+  for (const [key, chars] of Object.entries(keyboard)) {
+    if (chars.includes(char)) {
+      keypress = key;
+      times = chars.indexOf(char) + 1;
+      break;
+    }
+  }
+  
+  return keypress.repeat(times);
+}
+
+const sendMessage = message => {
+  // Start typing here
+  let lastBtn = '';
+  let isLastUpperCase = false; 
+  
+  const keyboard = {
+    '1': '.,?!',
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+    '0': ' ',
+    '*': "'-+=",
+    '#': '@',
+  }
+  
+  let result = '';
+  
+  for (let i = 0; i < message.length; i++) {
+    const char = message[i];
+
+    if(char.match(/[a-zA-Z]/) && isLowerCase(char) && isLastUpperCase) {
+      result += '#';
+      isLastUpperCase = false;
+    }
+    
+    if(char.match(/[a-zA-Z]/) && !isLowerCase(char) && !isLastUpperCase) {
+      result += '#';
+      isLastUpperCase = true;
+    }
+    
+    lastBtn = result[result.length - 1];
+    
+    if(keyboard[char]) {
+      if (char === lastBtn) {
+        result += ' ';
+      }
+      result += char + '-';
+    } else {   
+      const key = getKeyPress(char, keyboard);
+      result += key[0] === lastBtn ? ' ' + key : key;
+    }
+    
+    
+  }
+  
+  return result;
+}
