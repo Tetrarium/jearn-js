70579ff215bcb2cd969ab1e7dc5fa3ea89dea32b
AlexandrTurov <alexandr.turov@mail.ru>
2024-07-23 17:42:27 +0700

    Add Chapter 6.4

---
+++ chapter06/ch06_04/main.js
@@ -0,0 +1,103 @@
+console.log('Chapter 6.3');
+
+// function sayHi() {
+//   var phrase = 'Hello';
+
+//   console.log(phrase);
+// }
+
+// sayHi();
+// console.log(phrase);
+
+
+// if (true) {
+//   var test = true;
+// }
+
+// console.log(test);
+
+// if (true) {
+//   let test = true;
+// }
+// console.log(test);
+
+
+// for (var i = 0; i < 10; i++) {
+
+// }
+
+// console.log(i);
+
+
+// function sayHi() {
+//   if (true) {
+//     var phrase = 'Hello';
+//   }
+
+//   console.log(phrase);
+// }
+
+// sayHi();
+// console.log(phrase);
+
+
+// let user;
+// let user;
+
+// var user = 'Uasya';
+// var user;
+// console.log(user);
+
+// var user = 'Petya';
+// let user = 'Uasya';
+// console.log(user);
+
+// var user = 'Uasya';
+// var user = 'Petya';
+// console.log(user);
+
+
+// function sayHi() {
+//   'use strict';
+//   console.log(phrase);
+
+//   phrase = 'Hello';
+
+//   console.log(phrase);
+
+//   if (false) {
+//     var phrase;
+//   }
+// }
+// sayHi();
+
+
+// (function() {
+//   var message = 'hello!';
+
+//   console.log(message);
+// })();
+
+
+// function() {
+//   var message = 'Hello';
+
+//   console.log(message);
+// }
+
+
+// function go() {
+
+// }();
+
+// (function() {
+//   console.log('any');
+// })();
+
+// !function() {
+//   console.log('negation');
+// }();
+
+// console.log(+function() {
+//   console.log('plus');
+// }())
