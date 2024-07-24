7cc568f226bc777e9989895622852f7abc852660
AlexandrTurov <alexandr.turov@mail.ru>
2024-07-24 17:16:05 +0700

    Add Chapter 6.7

---
+++ chapter06/ch06_07/main.js
@@ -0,0 +1,31 @@
+console.log('Chapter 6.6');
+
+// const sum = new Function('a', 'b', 'return a + b');
+
+// console.log(sum(1, 2));
+// console.log(sum(5, 9));
+
+// const sayHi = new Function('console.log("Hello")');
+// sayHi();
+
+// function getFunc() {
+//   const value = 'test';
+
+//   const func = new Function('console.log(value)');
+
+//   return func;
+// }
+
+// const value = 'new test';
+// getFunc()();
+
+
+function getFunc() {
+  const value = 'test';
+
+  const func = function() {console.log(value)};
+
+  return func;
+}
+
+getFunc()();
