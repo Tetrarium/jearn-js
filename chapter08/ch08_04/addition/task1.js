4f45f4ac19e24887e50bae3174bc47e824040fee
AlexandrTurov <alexandr.turov@mail.ru>
2024-08-30 14:05:11 +0700

    LearnJS 8.4 Add example with prototype

--- chapter08/ch08_04/addition/task1.js
+++ chapter08/ch08_04/addition/task1.js
@@ -3,9 +3,37 @@ function Test() {
 }
 
 function Test2() {
-  Test.call(this);
+  // Test.call(this);
 }
 
 Test2.prototype.__proto__ = Test.prototype;
 
 console.log(new Test2().test);
+
+
+
+Function.prototype.myCall = function (ctx, ...args) {
+  // const foo = this;
+
+  const myFoo = Symbol('foo');
+
+  ctx[myFoo] = this;
+
+  const res = ctx[myFoo](...args);
+
+  delete ctx[myFoo];
+  return res;
+}
+
+function Test3() {
+  Test.myCall(this);
+}
+
+Test3.prototype.__proto__ = Test.prototype;
+
+console.log(new Test3().test);
+console.log((new Test3).__proto__ );
+console.dir(new Test3());
+
+
+
